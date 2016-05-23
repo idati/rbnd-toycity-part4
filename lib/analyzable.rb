a04f9178078fce895fcd85097778014d4afb4ba2
Alecia_Gunter Specht <Alecia_Gunter Specht>
2016-05-23 16:40:33 +0200

    add count_by_brand

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -6,4 +6,10 @@ module Analyzable
   	(result/input.length).round(2)
   end
 
+  def count_by_brand(input)
+  	count=0
+  	input.each{|item, variable| count+=1}
+  	Hash.[]("#{input[0].brand}"=>count)
+  end
+
 end
