0f5f7d3659064ab9557ea1cae9fd6e77210e27d2
Alecia_Gunter Specht <Alecia_Gunter Specht>
2016-05-23 18:02:11 +0200

    add count_by_name

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -11,5 +11,10 @@ module Analyzable
   	input.each{|item, variable| count+=1}
   	Hash.[]("#{input[0].brand}"=>count)
   end
+  def count_by_name(input)
+  	count=0
+  	input.each{|item, variable| count+=1}
+  	Hash.[]("#{input[0].name}"=>count)
+  end
 
 end
